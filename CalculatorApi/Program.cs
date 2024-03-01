868626a7d6870610622593c59f96162ea9fd5742
Vincent Wang <vincentdenghui@gmail.com>
2024-03-01 13:16:32 +1100

    Add /calculator Endpoint

--- CalculatorApi/Program.cs
+++ CalculatorApi/Program.cs
@@ -1,7 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/calculator/{op}",  (string op, double operandA, double operandB) => {
+      double result = double.NaN;
+      try{
+            result = op switch{
+                "add" => operandA + operandB,
+                "subtract" => operandA - operandB,
+                "multiply" => operandA * operandB,
+                "divide" => operandA / operandB,
+                _ => throw new ArgumentException()
+            };
+        }catch (ArgumentException){
+                return Results.BadRequest($"bad operator:{op}");
+        }
+        return Results.Ok($"{result}");
+});
+
 
 app.Run();
 
